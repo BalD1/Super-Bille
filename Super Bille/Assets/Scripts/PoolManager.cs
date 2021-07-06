e76d9947746c99d0abf48d87659f236eea041d75
florian <florianhusson@hotmail.fr>
2021-07-06 09:05:32 +0200

    boot pool

--- Super Bille/Assets/Scripts/PoolManager.cs
+++ Super Bille/Assets/Scripts/PoolManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
+    [System.Serializable]
+    public class Pool
+    {
+        public tags tag;
+        public List<GameObject> prefabs;
+        public int size;
+    }
+
     private static PoolManager instance;
     public static PoolManager Instance
     {
         get
         {
             if(instance == null)
-                Debug.LogError("PoolManager instance not found");
+                Debug.LogError("PoolManager Instance not found");
 
             return instance;
         }
@@ -20,4 +28,50 @@ public class PoolManager : MonoBehaviour
     {
         instance = this;
     }
+
+    public enum tags
+    {
+        Tiles,
+    }
+
+    public List<Pool> pools;
+    public Dictionary<tags, Queue<GameObject>> poolDictionnary;
+
+    private void Start()
+    {
+        poolDictionnary = new Dictionary<tags, Queue<GameObject>>();
+
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for(int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefabs[Random.Range(0, pool.prefabs.Capacity)]);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionnary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(tags tag, Vector3 position, Quaternion rotation)
+    {
+        if(!poolDictionnary.ContainsKey(tag))
+        {
+            Debug.LogError("Pool with tag " + tag + " doesn't exist");
+            return null;
+        }
+        GameObject objToSpawn = poolDictionnary[tag].Dequeue();
+
+        objToSpawn.SetActive(true);
+        objToSpawn.transform.position = position;
+        objToSpawn.transform.rotation = rotation;
+
+        poolDictionnary[tag].Enqueue(objToSpawn);
+        return objToSpawn;
+    }
+
+
 }
