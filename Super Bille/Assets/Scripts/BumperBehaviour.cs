c4b251274c29d6bd249c7c06d56c9d1575b8dd70
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2021-07-06 17:13:06 +0200

    Circuit + Obstacles

---
+++ Super Bille/Assets/Scripts/BumperBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperBehaviour : MonoBehaviour
+{
+    public int speed;
+    public bool mirror; //pour inverser le direction de départ
+    Vector3 startPosition;
+    Renderer rend;
+    public float timer;
+    float time;
+
+    private void Start()
+    {
+        time = timer;
+        rend = GetComponent<Renderer>();
+        startPosition = this.transform.position;
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time <0)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 position = this.transform.position;
+        position.x += speed * Time.deltaTime;
+        this.transform.position = position;
+        //pour inverser le sens de déplacement
+        if (mirror)
+        {
+            if (position.x < startPosition.x - rend.bounds.size.x)
+            {
+                speed *= -1;
+            }
+            if (position.x > startPosition.x)
+            {
+                speed *= -1;
+                time = timer;
+            }
+        }
+        else
+        {
+            if (position.x > startPosition.x + rend.bounds.size.x)
+            {
+                speed *= -1;
+            }
+            if (position.x < startPosition.x)
+            {
+                speed *= -1;
+                time = timer;
+            }
+        }
+    }
+}
---
+++ Super Bille/Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour
+{
+    public Vector3 force;
+    int layerMask = 1 << 8;
+    private void Start()
+    {
+        layerMask = ~layerMask;
+    }
+    private void Update()
+    {
+        RaycastHit hit;
+        Vector3 positionRay = transform.position;
+        positionRay.y += 5;
+        Debug.DrawRay(transform.position,transform.forward * 10, Color.red);
+        Debug.DrawRay(transform.position,-transform.forward * 10, Color.red);
+        Debug.DrawRay(transform.position,transform.right * 10, Color.red);
+        Debug.DrawRay(transform.position,-transform.right * 10, Color.red);
+        if(Physics.Raycast(transform.position, transform.forward, out hit,10,layerMask))
+        {
+            force.y = Mathf.Abs(force.z);
+        }
+        if (Physics.Raycast(transform.position, -transform.forward, out hit, 10, layerMask))
+        {
+            force.z *= -1;
+        }
+        if (Physics.Raycast(transform.position, transform.right, out hit, 10, layerMask))
+        {
+            force.y = Mathf.Abs(force.x);
+        }
+        if (Physics.Raycast(transform.position, -transform.right, out hit, 10, layerMask))
+        {
+            force.x *= -1;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(force);
+    }
+}
