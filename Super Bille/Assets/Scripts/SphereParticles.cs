0275d4e879d29f74ddad9679b776d3ac30ab1f3a
florian <florianhusson@hotmail.fr>
2021-07-07 11:53:08 +0200

    petits effets et rangement

---
+++ Super Bille/Assets/Scripts/SphereParticles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereParticles : MonoBehaviour
+{
+    [SerializeField] private float requiredVelocity;
+    [SerializeField] private Rigidbody sphereRB;
+    [SerializeField] private ParticleSystem particles;
+
+    private void Start()
+    {
+        if(sphereRB == null)
+            sphereRB = GameObject.FindGameObjectWithTag("sphere").GetComponent<Rigidbody>();
+        if(particles == null)
+            particles = this.gameObject.GetComponent<ParticleSystem>();
+        
+    }
+
+    private void Update()
+    {
+        this.transform.position = sphereRB.transform.position;
+        Debug.Log(sphereRB.velocity);
+        if(sphereRB.velocity.z >= requiredVelocity && !particles.isPlaying)
+        {
+            particles.Play();
+        }
+        else if (sphereRB.velocity.z < requiredVelocity)
+        {
+            particles.Stop();
+        }
+        
+    }
+
+}
